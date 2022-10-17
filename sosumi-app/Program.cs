654d66739ae47a962c81880e376bcf45bc51ce60
Michael Closky <mikeclosky29@gmail.com>
2022-10-17 19:48:53 -0400

    added dependency injection

--- sosumi-app/Program.cs
+++ sosumi-app/Program.cs
@@ -1,3 +1,6 @@
+using sosumi_app.Interfaces;
+using sosumi_app.Repositories;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +9,9 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.AddTransient<IItemRepository, ItemRepository>();
+builder.Services.AddTransient<IOrderRepository, OrderRepository>();
+builder.Services.AddTransient<IUserRepository, UserRepository>();
 
 var app = builder.Build();
 
