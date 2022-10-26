47eaa4dad606eb1d12373e64db664cef3e470bec
macgere <mac_gere@yahoo.com>
2022-10-25 20:37:07 -0500

    Added GetOrdersByUserId()

--- sosumi-app/Interfaces/IOrderRepository.cs
+++ sosumi-app/Interfaces/IOrderRepository.cs
@@ -1,6 +1,9 @@
-﻿namespace sosumi_app.Interfaces
+﻿using sosumi_app.Models;
+
+namespace sosumi_app.Interfaces
 {
     public interface IOrderRepository
     {
-    }
+        public List<Order> GetOrdersByUserId(int id);
+     }
 }
