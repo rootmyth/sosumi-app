5fc945fc0e85cd300af5203cf1ddc0e022ebd11d
mckenziebyrdgranda <mckenziebyrd@me.com>
2022-11-12 12:26:57 -0600

    nav bar

--- sosumi-frontend/src/pages/Authenticated.js
+++ sosumi-frontend/src/pages/Authenticated.js
@@ -1,26 +1,26 @@
 import { React, useEffect, useState } from 'react';
 import { signOut } from '../utils/auth';
-import { Navbar } from '../components/Navbar/Navbar';
+
 
 export default function Authenticated({ user }) {
   const [items, setItems] = useState([]);
-  // useEffect(() => {
-  //   fetch(
-  //     'https://localhost:7283/api/Item',
-  //     {
-  //       method: 'GET',
-  //       headers: {
-  //         'Access-Control-Allow-Origin': 'https://localhost:7283',
-  //         'Content-Type': 'application/json',
-  //       },
-  //     },
-  //   )
-  //     .then((res) => res.json())
-  //     .then((r) => {
-  //       setItems(r);
-  //       console.table(r);
-  //     });
-  // }, []);
+  useEffect(() => {
+    fetch(
+      'https://localhost:7283/api/Item',
+      {
+        method: 'GET',
+        headers: {
+          'Access-Control-Allow-Origin': 'https://localhost:7283',
+          'Content-Type': 'application/json',
+        },
+      },
+    )
+      .then((res) => res.json())
+      .then((r) => {
+        setItems(r);
+        console.table(r);
+      });
+  }, []);
   return (
     <div className="text-center mt-5">
   
@@ -35,10 +35,10 @@ export default function Authenticated({ user }) {
           Sign Out
         </button>
       </div>
-      {/* <div>
+      <div>
         <h1>item list below</h1>
         <h2>{items.map((x) => x.name)}</h2>
-      </div> */}
+      </div>
     </div>
   );
 }
