e482a4803e62c652d0c382fe50c9450a88445cc8
mckenziebyrdgranda <mckenziebyrd@me.com>
2022-11-12 12:28:34 -0600

    nav bar user menu

--- sosumi-frontend/src/components/Navbar/Navbar.js
+++ sosumi-frontend/src/components/Navbar/Navbar.js
@@ -1,14 +1,18 @@
 import React, {useState} from "react"
 import "./Navbar.css"
 import {FaBars, FaTimes } from "react-icons/fa";
+import {BsPersonCircle} from "react-icons/bs"
 import { Link } from "react-router-dom";
 import { SidebarMenuData } from "./SidebarMenuData";
 import { IconContext } from 'react-icons'
+import { userSidebarData } from "./userSidebarData";
 
 const Navbar = () => {
    const [sidebar, setSidebar] = useState(false)
+   const [userSidebar, setuserSidebar] = useState(false)
 
    const showSidebar = () => setSidebar(!sidebar)
+   const showUserSidebar = () => setuserSidebar(!userSidebar)
 
     return (
         <>
@@ -17,7 +21,7 @@ const Navbar = () => {
         <Link to="#" className='menu-bars'>
             <FaBars onClick={showSidebar}/>
         </Link>
-        <h2>SoSumi</h2>
+        
         <nav className={sidebar ? 'nav-menu active' : 'nav-menu'}>
             <ul className="nav-menu-items" onClick={showSidebar}>
                 <li className="navbar-toggle">
@@ -37,7 +41,31 @@ const Navbar = () => {
                 })}
             </ul>
         </nav>
-     
+     <h2 className="title">SoSumi</h2>
+
+     <Link to="#" className='menu-bars'>
+            <BsPersonCircle onClick={showUserSidebar}/>
+        </Link>
+        
+        <nav className={userSidebar ? 'nav-menu active' : 'nav-menu'}>
+            <ul className="nav-menu-items" onClick={showUserSidebar}>
+                <li className="nav-navbar-toggle">
+                    <Link to="/#" className="menu-bars">
+                        <FaTimes />
+                    </Link>
+                </li>
+                {userSidebarData.map((item, index) => {
+                    return (
+                        <li key={index} className={item.className}>
+                            <Link to={item.path}>
+                                {item.icon}
+                                <span>{item.title}</span>
+                            </Link>
+                        </li>
+                    )
+                })}
+            </ul>
+        </nav>
       </nav>
       </IconContext.Provider>
       </>
--- sosumi-frontend/src/routes/index.js
+++ sosumi-frontend/src/routes/index.js
@@ -1,15 +1,15 @@
 // index for router
 import React from 'react';
 import { Route, Switch } from 'react-router-dom';
-import Navbar from '../components/Navbar/Navbar';
 import Authenticated from '../pages/Authenticated';
+import Navbar from '../components/Navbar/navbar'
 
 export default function Routes({ user }) {
   return (
     <div>
       <Navbar />
       <Switch>
-        <Route path="/" component={() => <Authenticated user={user} />} />
+        <Route exact path="/" component={() => <Authenticated user={user} />} />
         <Route path="*" component={() => <Authenticated user={user} />} />
       </Switch>
     </div>
