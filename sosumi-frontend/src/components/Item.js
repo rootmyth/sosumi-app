6746506232af9d1e57bf0e6be95d3f51d2e4976f
Michael Closky <mikeclosky29@gmail.com>
2022-11-14 19:10:47 -0500

    changes for mork

--- sosumi-frontend/src/components/Item.js
+++ sosumi-frontend/src/components/Item.js
@@ -1,11 +1,43 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
+import '../styles/item.css'
 
-export default function Item({item}) {
+export default function Item({item, favorited}) {
     const {name, price} = item;
+    const [fav, setFav] = useState(favorited);
+    
+    const handleOnClick = () => {
+      if(fav){
+        fetch(
+          'https://localhost:7283/api/Favorite/deleteFavorite/7/'+item.id,
+          {
+            method: 'DELETE',
+            headers: {
+              'Access-Control-Allow-Origin': 'https://localhost:7283',
+              'Content-Type': 'application/json',
+            },
+          },
+        )
+      } else {
+        fetch(
+          'https://localhost:7283/api/Favorite/addFavorite/7/'+item.id,
+          {
+            method: 'POST',
+            headers: {
+              'Access-Control-Allow-Origin': 'https://localhost:7283',
+              'Content-Type': 'application/json',
+            },
+            body:""
+          },
+        )
+      }
+      setFav(!fav);
+    }
+
   return (
     <>
     <div>
         Name: {name}  Price: {price} 
+        <button onClick={handleOnClick} className={favorited? "favorite" : "not favorite"}>{fav ? "Delete from favorite" : "add to favorite"}</button>
     </div>
     </>
   )
--- sosumi-frontend/src/components/ItemList.js
+++ sosumi-frontend/src/components/ItemList.js
@@ -1,10 +1,20 @@
 import React from 'react'
 import  Item  from './Item'
 
-export default function ItemList({items}) {
+export default function ItemList({items, favList}) {
+  const checkIfFavorited = (id) => {
+    for(const item of favList){
+      console.log("itemid:  " + item.id + "   id:  " + id)
+      if(item.id == id){
+        return true;
+      }
+      return false;
+    }
+  }
+
   return (
     <div>
-        {items.map(item => <Item key={item.id} item={item}/>)}
+        {items.map(item => <Item key={item.id} item={item} favorited={() => checkIfFavorited(item.id)}/>)}
     </div>
   )
 }
